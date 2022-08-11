898dc041a902000e8ff3130e3735452c2ce7dd33
Florian <107463310+efloriangit@users.noreply.github.com>
2022-08-11 15:44:34 +0200

    implemented boids

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ exhibit swarm-like behavior which emerges from three simple rules:
 """
 
 # Imports
+# import argparse
+
+
+
+from lib.initializeBoids import boid
+
+import pygame
 import argparse
-
-import moderngl
-import imgui
-# from ... import ...
-
 
 
 def parse_cmd_arguments(default=False, argv=None):
@@ -36,11 +38,49 @@ def parse_cmd_arguments(default=False, argv=None):
         (argparse.Namespace): The Namespace object containing argument names and
             values.
     """
+
+
+
+    parser = argparse.ArgumentParser(description='boids simulation', \
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    ggroup = parser.add_argument_group('General settings')
+    ggroup.add_argument('--windowWidth', type = float, default = 1800,
+                        help = 'Width of the pygame window in pixels')
+    ggroup.add_argument('--windowHeight', type = float, default = 1200,
+                        help = 'Height of the pygame window in pixels')
+    ggroup.add_argument('--maxFPS', type = float, default = 60,
+                        help = 'Maximum of frames per second')
+    ggroup.add_argument('--nBoids', type = int, default = 80,
+                        help = 'number of boids to be created')
+
+    bgroup = parser.add_argument_group('Boid settings')
+    bgroup.add_argument('--color01', type = tuple, default = (120, 120, 40),
+                        help = 'color01 of the boids')
+    bgroup.add_argument('--velocity', type = float, default = 6,
+                        help = 'movement velocity')
+    bgroup.add_argument('--separationDistance', type = float, default = 20,
+                        help = 'maximum distance for the separation force to take effect')
+    bgroup.add_argument('--fieldOfVisionRadius', type = float, default = 50,
+                        help = 'maximum distance for all forces to take effect')
+    bgroup.add_argument('--alignmentStrength', type = float, default = 1,
+                        help = 'weigthing of the alignment vector')
+    bgroup.add_argument('--separationStrength', type = float, default = 1.5,
+                        help = 'weigthing of the separation vector')
+    bgroup.add_argument('--cohesionStrength', type = float, default = 1,
+                        help = 'weigthing of the cohesion vector')
+    bgroup.add_argument('--weightingPreviousMovementVector', type = float, default = 0.96,
+                        help = 'weighting of the previous movement vector; lower values result in more agility')
+
+
+
+    args = parser.parse_args()
+
     return args
 
 
 
-def run(visualize=True):
+def run():
     """Runs the script.
 
     The :mod:`main.run` method performs the following actions:
@@ -54,44 +94,42 @@ def run(visualize=True):
         visualize (boolean): Whether to create a visualization.
     """
 
-    script_start = time()
 
-    ### Parse command line arguments.
+
+    # Parse command line arguments.
     args = parse_cmd_arguments()
 
-    ### create a set of boids
-    initializeBoids(args)
+    # open a pygame window
+    pygame.init()
+    window = pygame.display.set_mode([args.windowWidth, args.windowHeight])
 
-    ### initialize visualization
-    initializeVisualization(args)
-    # for each timestep {
+    clock = pygame.time.Clock()
+    running = True
 
-    ### calculate boid positions for the next timestep
-    iterateBoids(boidsData)
+    # create a set of boids
+    swarm = [boid(args) for i in range(args.nBoids)]
 
-    ### update visualization
-    iterateVisualization(boidsData)
-    #}
+    while running:
+        clock.tick(args.maxFPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-
-
-
-
-
-
+        # empty the screen
+        window.fill((0, 0, 0))
 
 
+        ### update boids
+        for i in swarm:
+            i.move(args, window, swarm)
+            i.drawShape(args, window)
+
+        ### flip the screen
+        pygame.display.flip()
 
 
-
-
-
-
-
-
-
-
-
+    ### close
+    pygame.quit()
 
 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-# This is going to be a simple implementation and visualization of boids.
+# This is a simple implementation and visualization of boids.
+
+# Boids are objects that exhibit swarm behavior by individually moving according
+# to 3 rules:
+
+# 1) alignment
+# 2) separation
+# 3) cohesion
+
+# An additional rule ensures that they stay within the boundaries of the window.
+
+# To run, execute main.py and adjust boid behavior by setting the according flags
+
+# Further information can be obtained with main.py --help
